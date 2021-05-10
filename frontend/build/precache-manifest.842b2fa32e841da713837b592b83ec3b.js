6fec4b1dbf3985891e89325952244fb2745a6502
sairabbas <sair_abbas@yahoo.com>
2021-05-10 01:25:05 -0700

    AWS Amp

---
+++ frontend/build/precache-manifest.842b2fa32e841da713837b592b83ec3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6418200c1f6ca7cc8e9a4d1d27e8ec95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd390b6114df6b0d50c3",
+    "url": "/static/css/2.d150562a.chunk.css"
+  },
+  {
+    "revision": "62aa329e809e01c28b28",
+    "url": "/static/css/main.b7737da8.chunk.css"
+  },
+  {
+    "revision": "dd390b6114df6b0d50c3",
+    "url": "/static/js/2.fbdb7986.chunk.js"
+  },
+  {
+    "revision": "62aa329e809e01c28b28",
+    "url": "/static/js/main.5419cce3.chunk.js"
+  },
+  {
+    "revision": "9ab59728f7d8b149b378",
+    "url": "/static/js/runtime-main.8de28bae.js"
+  },
+  {
+    "revision": "82eb6e8a34c1a2f4348d34943e651611",
+    "url": "/static/media/SpaceDoggo.82eb6e8a.png"
+  }
+]);
